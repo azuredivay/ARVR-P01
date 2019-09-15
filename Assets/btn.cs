7d2a8704205417ac180c9d8add11901b957de8b4
divay pandey <divay.pandey@outlook.com>
2019-09-15 00:47:57 -0500

    year of publish

---
+++ Assets/btn.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class btn : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public AudioClip MusicClip;
+    public AudioSource MusicSource;
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        Debug.Log("PRESSED");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        Debug.Log("REL");
+        MusicSource.Play();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MusicSource.clip = MusicClip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
